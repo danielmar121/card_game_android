6acde0b2af480bfc8e9f6074a0b13eb2757754c3
Daniel <53561824+danielmar121@users.noreply.github.com>
2020-11-20 16:29:49 +0200

    Sound, timer and progress bar(#6)
    * Change button from ImageView to ImageButton
    
    * Add timer functionality
    
    * Add clock sound and win sound
    
    * Add progress bar!

--- app/src/main/java/com/daniel/card_game_android/MainActivity.java
+++ app/src/main/java/com/daniel/card_game_android/MainActivity.java
@@ -1,23 +1,35 @@
 package com.daniel.card_game_android;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    static final int SECOND = 1000;
     private final int NUMBER_OF_CARDS = 26;
     Deck warDeck;
     TextView main_LBL_score_player_A, main_LBL_score_player_B;
     ImageView main_IMG_player_A_card, main_IMG_player_B_card;
-    ImageView main_BTN_play ;
+    ImageButton main_BTN_play ;
     private int playerScoreA = 0,  playerScoreB = 0;
-
+    private Timer carousalTimer;
+    private Sound tickingSound;
+    private ProgressBar main_PGR_game_progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         main_IMG_player_A_card = findViewById(R.id.main_IMG_player_A_card);
         main_IMG_player_B_card = findViewById(R.id.main_IMG_player_B_card);
         main_BTN_play = findViewById(R.id.main_BTN_play);
+        tickingSound = new Sound(this, R.raw.ticking_clock_sound);
+        main_PGR_game_progress = findViewById(R.id.main_PGR_game_progress);
     }
 
     private void initViews() {
@@ -41,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
         main_BTN_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                playTurn();
+                main_BTN_play.setEnabled(false);
+                startCounting();
             }
         });
     }
@@ -61,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         setNewCardsImage(playerCardA.getImageName(),playerCardB.getImageName());
 
         setScore(playerCardA,playerCardB);
+
+        setProgress();
 
         if(warDeck.isEmpty()){
             displayWinner();
@@ -85,11 +102,77 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void setProgress() {
+        double sizeOfBar = 100/(NUMBER_OF_CARDS/2.0);
+        double totalTurns = playerScoreA + playerScoreB;
+        int gameProgress = (int)(totalTurns * sizeOfBar);
+        main_PGR_game_progress.setProgress(gameProgress);
+    }
+
     private void displayWinner() {
         Intent intent = new Intent(this, WinnerPage.class);
         intent.putExtra(WinnerPage.playerScoreA, "" + playerScoreA);
         intent.putExtra(WinnerPage.playerScoreB, "" + playerScoreB);
         startActivity(intent);
         finish();
+    }
+
+    private void startCounting() {
+        carousalTimer = new Timer();
+        carousalTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        playTurn();
+                        tickingSound.playSound();
+                    }
+                });
+            }
+        }, 0, 1*SECOND);
+    }
+
+    private void stopCounting() {
+        carousalTimer.cancel();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("pttt","onStart");
+        if (carousalTimer != null){
+            startCounting();
+            tickingSound.setSound(this, R.raw.ticking_clock_sound);
+            tickingSound.playSound();
+        }
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("pttt","onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("pttt","onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("pttt","onStop");
+        if (carousalTimer != null){
+            stopCounting();
+            tickingSound.stopSound();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("pttt", "onDestroy");
+        super.onDestroy();
     }
 }
