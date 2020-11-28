e5f3b5efd24ae455ac4563d1cd468cd9c5547eeb
Daniel <53561824+danielmar121@users.noreply.github.com>
2020-11-28 19:39:26 +0200

    Revert "Records page" (#11)

--- app/src/main/java/com/daniel/card_game_android/MainActivity.java
+++ app/src/main/java/com/daniel/card_game_android/MainActivity.java
@@ -1,25 +1,21 @@
 package com.daniel.card_game_android;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends ActivityBase {
-    public static final String gander = "GANDER";
-    public static final String name = "NAME";
-
-    private final int SECOND = 1000;
+public class MainActivity extends AppCompatActivity {
     private final int NUMBER_OF_CARDS = 26;
     Deck warDeck;
-    String playerGander;
+    TextView main_LBL_score_player_A, main_LBL_score_player_B;
+    ImageView main_IMG_player_A_card, main_IMG_player_B_card;
+    Button main_BTN_play;
     private int playerScoreA = 0, playerScoreB = 0;
-    private String playerName;
-    private Timer carousalTimer;
-    private MainViewController mainViewController;
 
 
     @Override
@@ -27,19 +23,29 @@ public class MainActivity extends ActivityBase {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mainViewController = new MainViewController(this);
-
+        findViews();
         initViews();
+    }
 
-        isDoubleBackPressToClose = true;
+    private void findViews() {
+        main_LBL_score_player_A = findViewById(R.id.main_LBL_score_player_A);
+        main_LBL_score_player_B = findViewById(R.id.main_LBL_score_player_B);
+        main_IMG_player_A_card = findViewById(R.id.main_IMG_player_A_card);
+        main_IMG_player_B_card = findViewById(R.id.main_IMG_player_B_card);
+        main_BTN_play = findViewById(R.id.main_BTN_play);
     }
 
     private void initViews() {
         initDeck();
-        setPlayerGanderAndName();
+        main_BTN_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playTurn();
+            }
+        });
     }
 
-    public void initDeck() {
+    private void initDeck() {
         warDeck = new Deck();
         for (int i = 1; i <= NUMBER_OF_CARDS; i++) {
             warDeck.addCard("card_" + i, i);
@@ -47,129 +53,42 @@ public class MainActivity extends ActivityBase {
         warDeck.shuffleCards();
     }
 
-    private void setPlayerGanderAndName() {
-        Intent intent = getIntent();
-        playerGander = intent.getStringExtra(gander);
-        playerName = intent.getStringExtra(name);
-
-
-        if (playerGander.matches("girl")) {
-            int playerGirl = this.getResources().getIdentifier("player_girl", "drawable", this.getPackageName());
-            Drawable girlImage = getDrawable(playerGirl);
-            mainViewController.setPlayerImage(girlImage);
-            mainViewController.setPlayerCardImage(girlImage);
-        }
-    }
-
     private void playTurn() {
         Card playerCardA = warDeck.getCard();
         Card playerCardB = warDeck.getCard();
-        if (playerCardA != null && playerCardB != null) {
-            setNewCardsImage(playerCardA.getImageName(), playerCardB.getImageName());
 
-            setScore(playerCardA, playerCardB);
+        setNewCardsImage(playerCardA.getImageName(), playerCardB.getImageName());
 
-            setProgress();
+        setScore(playerCardA, playerCardB);
 
-            if (warDeck.isEmpty()) {
-                displayWinner();
-            }
+        if (warDeck.isEmpty()) {
+            displayWinner();
         }
-
-
     }
 
     private void setNewCardsImage(String imageNameA, String imageNameB) {
         int playerDrawableA = this.getResources().getIdentifier(imageNameA, "drawable", this.getPackageName());
         int playerDrawableB = this.getResources().getIdentifier(imageNameB, "drawable", this.getPackageName());
 
-        mainViewController.setPlayerCardImage(getDrawable(playerDrawableA));
-        mainViewController.setComputerCardImage(getDrawable(playerDrawableB));
+        main_IMG_player_A_card.setImageDrawable(getDrawable(playerDrawableA));
+        main_IMG_player_B_card.setImageDrawable(getDrawable(playerDrawableB));
     }
 
     private void setScore(Card playerCardA, Card playerCardB) {
         if (playerCardA.isStronger(playerCardB)) {
             playerScoreA++;
-            mainViewController.setPlayerScore(playerScoreA + "");
+            main_LBL_score_player_A.setText(playerScoreA + "");
         } else {
             playerScoreB++;
-            mainViewController.setComputerScore(playerScoreB + "");
+            main_LBL_score_player_B.setText(playerScoreB + "");
         }
-    }
-
-    private void setProgress() {
-        double sizeOfBar = 100 / (NUMBER_OF_CARDS / 2.0);
-        double totalTurns = playerScoreA + playerScoreB;
-        int gameProgress = (int) (totalTurns * sizeOfBar);
-        mainViewController.setProgressBar(gameProgress);
     }
 
     private void displayWinner() {
         Intent intent = new Intent(this, WinnerPage.class);
-        intent.putExtra(WinnerPage.playerScoreA, playerScoreA);
-        intent.putExtra(WinnerPage.playerScoreB, playerScoreB);
-        intent.putExtra(MainActivity.gander, playerGander);
-        intent.putExtra(name, playerName);
+        intent.putExtra(WinnerPage.playerScoreA, "" + playerScoreA);
+        intent.putExtra(WinnerPage.playerScoreB, "" + playerScoreB);
         startActivity(intent);
         finish();
-    }
-
-    public void startCounting() {
-        Log.d("pttt", "startCounting");
-        carousalTimer = new Timer();
-        carousalTimer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        playTurn();
-                        mainViewController.playSound();
-                    }
-                });
-            }
-        }, 0, SECOND);
-    }
-
-    private void stopCounting() {
-        carousalTimer.cancel();
-    }
-
-    @Override
-    protected void onStart() {
-        Log.d("pttt", "onStart");
-        if (carousalTimer != null) {
-            startCounting();
-            mainViewController.playSound();
-        }
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d("pttt", "onResume");
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d("pttt", "onPause");
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d("pttt", "onStop");
-        if (carousalTimer != null) {
-            stopCounting();
-            mainViewController.stopSound();
-        }
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d("pttt", "onDestroy");
-        super.onDestroy();
     }
 }
